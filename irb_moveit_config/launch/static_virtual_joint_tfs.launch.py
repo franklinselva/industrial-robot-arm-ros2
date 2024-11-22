6f7a04afb7c5b2670ef6b97629de57ed2b0c50b1
franklinselva <franklinselva10@gmail.com>
2024-11-22 14:48:17 +0100

    Minor fixes to formatting

--- irb_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ irb_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -1,7 +1,10 @@
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_launch
+from moveit_configs_utils.launches import \
+    generate_static_virtual_joint_tfs_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("irb6640_205", package_name="irb_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        "irb6640_205", package_name="irb_moveit_config"
+    ).to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
--- irb_moveit_config/launch/warehouse_db.launch.py
+++ irb_moveit_config/launch/warehouse_db.launch.py
@@ -3,5 +3,7 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("irb6640_205", package_name="irb_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        "irb6640_205", package_name="irb_moveit_config"
+    ).to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
